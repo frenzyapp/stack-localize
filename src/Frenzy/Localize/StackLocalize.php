9ba6b195e7d3de23e92b9034528c758d628e6d09
Martin Bastien <martin.bastien@studiofrenetic.com>
2014-05-17 13:29:18 -0400

    add headers and query string on redirect
    Signed-off-by: Martin Bastien <martin.bastien@studiofrenetic.com>

--- src/Frenzy/Localize/StackLocalize.php
+++ src/Frenzy/Localize/StackLocalize.php
@@ -7,69 +7,72 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class StackLocalize implements HttpKernelInterface {
 
-	/**
-	 * An array of valid locales.
-	 * 
-	 * @var array
-	 */
-	protected $locales;
+    /**
+     * An array of valid locales.
+     *
+     * @var array
+     */
+    protected $locales;
 
-	/**
-	 * The default locale.
-	 * 
-	 * @var string
-	 */
-	protected $defaultLocale;
+    /**
+     * The default locale.
+     *
+     * @var string
+     */
+    protected $defaultLocale;
 
-	/**
-	 * Constructor.
-	 * 
-	 * @param HttpKernelInterface $app
-	 * @param array               $locales
-	 * @param string              $defaultLocale
-	 */
-	public function __construct(HttpKernelInterface $app, array $locales = null, $defaultLocale)
-	{
-		$this->app = $app;
-		$this->locales = $locales;
-		$this->defaultLocale = $defaultLocale;
-	}
+    /**
+     * Constructor.
+     *
+     * @param HttpKernelInterface $app
+     * @param array               $locales
+     * @param string              $defaultLocale
+     */
+    public function __construct(HttpKernelInterface $app, array $locales = null, $defaultLocale)
+    {
+        $this->app = $app;
+        $this->locales = $locales;
+        $this->defaultLocale = $defaultLocale;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = true)
-	{
-		// Check for the default locale.
-		$default = $this->defaultLocale ?: $request->getPreferredLanguage($this->locales);
-		
-		// URI prefix lookup.
-		$locale = explode('/', trim($request->getPathInfo(), '/'));
-		$locale = array_filter($locale, function($v) { return $v != ''; });
-		$locale = isset($locale[0]) ? $locale[0] : $default;
+    /**
+     * {@inheritDoc}
+     */
+    public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = true)
+    {
+        // Check for the default locale.
+        $default = $this->defaultLocale ?: $request->getPreferredLanguage($this->locales);
 
-		$isValidLocale = in_array($locale, $this->locales);
-		if ( ! $isValidLocale) $locale = $default;
+        // URI prefix lookup.
+        $locale = explode('/', trim($request->getPathInfo(), '/'));
+        $locale = array_filter($locale, function($v) { return $v != ''; });
+        $locale = isset($locale[0]) ? $locale[0] : $default;
 
-		$pathInfo = $request->getPathInfo();
+        $isValidLocale = in_array($locale, $this->locales);
+        if ( ! $isValidLocale) $locale = $default;
 
-		// If the locale in the URI is the default, redirect to URI without locale.
-		if ($isValidLocale and $locale === $default and $pathInfo !== '/')
-		{
-			$redirect = '/'.ltrim(substr($pathInfo, strlen($locale) +1), '/');
-			return RedirectResponse::create($redirect, 301);
-		}
+        $pathInfo = $request->getPathInfo();
 
-		// Get the root path of the request.
-		$root = $request->server->get('SCRIPT_FILENAME');
+        // If the locale in the URI is the default, redirect to URI without locale.
+        if ($isValidLocale and $locale === $default and $pathInfo !== '/')
+        {
+            $redirect = '/'.ltrim(substr($pathInfo, strlen($locale) +1), '/');
 
-		// Duplicate the request so we can change the root path and detected locales.
-		$newRequest = $request->duplicate();
-		$newRequest->server->set('SCRIPT_FILENAME', $root . '/' . $locale);
-		$newRequest->setDefaultLocale($default);
-		$newRequest->setLocale($locale);
+            if ($request->getQueryString()) $redirect .= '?'.$request->getQueryString();
 
-		return $this->app->handle($newRequest, $type, $catch);
-	}
+            return RedirectResponse::create($redirect, 301, $request->headers);
+        }
+
+        // Get the root path of the request.
+        $root = $request->server->get('SCRIPT_FILENAME');
+
+        // Duplicate the request so we can change the root path and detected locales.
+        $newRequest = $request->duplicate();
+        $newRequest->server->set('SCRIPT_FILENAME', $root . '/' . $locale);
+        $newRequest->setDefaultLocale($default);
+        $newRequest->setLocale($locale);
+
+        return $this->app->handle($newRequest, $type, $catch);
+    }
 
 }
